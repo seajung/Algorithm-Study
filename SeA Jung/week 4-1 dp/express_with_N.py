66b192a01e2ec367c3f78e26f8f1d251039eac4d
seajung <seajh1114@gmail.com>
2022-01-24 09:51:37 +0900

    Time Complexity: O(1)

--- SeA Jung/week 4-1 dp/express_with_N.py
+++ SeA Jung/week 4-1 dp/express_with_N.py
@@ -1,23 +1,26 @@
 def solution(N, number):
-    possible_set = [0, [N]]
-    if N == number:
-        return 1
-    for i in range(2, 9):
-        case_set = []
-        basic_num = int(str(N)*i)
-        case_set.append(basic_num)
-        for i_half in range(1, i//2+1):
-            for x in possible_set[i_half]:
-                for y in possible_set[i-i_half]:
-                    case_set.append(x+y)
-                    case_set.append(x-y)
-                    case_set.append(y-x)
-                    case_set.append(x*y)
+    answer = -1
+    possible_num = [0]
+
+    for i in range(1, 9):
+        numbers = set()
+        # 단순 나열
+        numbers.add(int(str(N) * i))
+
+        for j in range(1, i):
+            for x in possible_num[j]:
+                for y in possible_num[i-j]:
+                    numbers.add(x + y)
+                    numbers.add(x - y)
+                    numbers.add(x * y)
+
                     if y != 0:
-                        case_set.append(x/y)
-                    if x != 0:
-                        case_set.append(y/x)
-            if number in case_set:
-                return i
-            possible_set.append(case_set)
-    return -1
+                        numbers.add(x // y)
+
+        if number in numbers:
+            answer = i
+            break
+
+        possible_num.append(numbers)
+
+    return answer
